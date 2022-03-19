00a46a1f0d28dc0fa5f0b51ec1390fe082911fac
Hendrik Schmidt <hendriknas@gmail.com>
2022-03-19 11:55:28 +0100

    move build to docs

--- svelte.config.js
+++ svelte.config.js
@@ -6,14 +6,12 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-	
-	kit: {
-		adapter: adapter(),
 
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		},
+	kit: {
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs',
+		}),
 
 		prerender: {
 			default: true
