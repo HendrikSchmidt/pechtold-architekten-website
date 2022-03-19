a32b948303508f7fe39c26fe563e7f96e5a0a7ea
Hendrik Schmidt <hendriknas@gmail.com>
2022-03-19 12:01:35 +0100

    test GH pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -13,9 +15,9 @@ const config = {
 			assets: 'docs',
 		}),
 
-		paths: {
-			base: dev ? '' : '/pechtold-architekten-website',
-		},
+		// paths: {
+		// 	base: dev ? '' : '/pechtold-architekten-website',
+		// },
 
 		prerender: {
 			default: true
