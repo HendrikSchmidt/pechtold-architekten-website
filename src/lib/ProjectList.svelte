cbab74aeb69e06d7d6151554d7da5bdcd5c00b37
Hendrik Schmidt <hendriknas@gmail.com>
2022-03-30 01:45:34 +0200

    new project list styling

--- src/lib/ProjectList.svelte
+++ src/lib/ProjectList.svelte
@@ -8,27 +8,27 @@
 <div class="row">
 	{#each projects as project}
 		<a
-			class="col-md-6 text-dark"
+			class="col-lg-6 text-dark"
 			sveltekit:prefetch
 			href="{base}/projekt/{sluggify(project.attributes.Titel)}"
 		>
 			<div class="card mb-3">
-				<div class="card-body">
-					<p class="duration card-text m-0">
-						<small class="text-muted">{project.attributes.Dauer}</small>
-					</p>
-					<h5 class="card-title m-0">{project.attributes.Titel}</h5>
-				</div>
 				{#if project.attributes.Fotos.data}
 					<div class="img-container">
 						<img
 							loading="lazy"
-							class="card-img-bottom"
+							class="card-img-top"
 							src={project.attributes.Fotos.data[0].attributes.formats.medium.url}
 							alt={project.attributes.Fotos.data[0].attributes.alternativeText}
 						/>
 					</div>
 				{/if}
+				<div class="card-body position-absolute bottom-0 text-white p-3 w-100">
+					<p class="duration card-text m-0">
+						<small>{project.attributes.Dauer}</small>
+					</p>
+					<h5 class="card-title m-0">{project.attributes.Titel}</h5>
+				</div>
 			</div>
 		</a>
 	{/each}
@@ -36,14 +36,18 @@
 
 <style lang="scss">
 	a {
-			text-decoration: none;
-		}
+		text-decoration: none;
+	}
+
+	.card-body {
+		background: rgba(50,50,50,0.7);
+	}
 
 	.img-container {
 		aspect-ratio: 3 / 2;
 		overflow: hidden;
 
-		.card-img-bottom {
+		img {
 			width: 100%;
 			height: 100%;
 			object-position: center center;
