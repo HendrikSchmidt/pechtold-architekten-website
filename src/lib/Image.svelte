7024de246263762d46e1c06571ff1408d44a889c
Hendrik Schmidt <hendriknas@gmail.com>
2022-05-11 14:55:38 +0200

    Update Image.svelte

--- src/lib/Image.svelte
+++ src/lib/Image.svelte
@@ -5,15 +5,11 @@
     export let sizes;
     export let classString = "";
     let { formats } = img;
-    console.log(formats);
-    console.log(formats[src].url);
-    console.log(formats.small.url);
 </script>
 
 <img
     class={classString}
     loading={lazy ? 'lazy' : 'eager'}
     src={formats[src] ? formats[src].url : formats.small.url}
-    {sizes}
     alt={img.alternativeText}
 />
