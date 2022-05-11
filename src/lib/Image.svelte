903c0e5613884a203b27a6f300ef40a015798889
Hendrik Schmidt <hendriknas@gmail.com>
2022-05-11 15:09:12 +0200

    refactor srcset

--- src/lib/Image.svelte
+++ src/lib/Image.svelte
@@ -5,11 +5,20 @@
     export let sizes;
     export let classString = "";
     let { formats } = img;
+    const scales = {
+        small: '500w',
+        medium: '750w',
+        large: '1000w',
+        xlarge: '1500w',
+    };
+    let srcset = Object.entries(scales).filter(([scale]) => formats[scale]).map(([scale, width]) => `${formats[scale].url} ${width}`).join();
 </script>
 
 <img
     class={classString}
     loading={lazy ? 'lazy' : 'eager'}
     src={formats[src] ? formats[src].url : formats.small.url}
+    {srcset}
+    {sizes}
     alt={img.alternativeText}
 />
